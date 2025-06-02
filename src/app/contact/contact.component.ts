436e87f872dbf2835169ff5f587ab8ffdd5e8a06
NirajBothe2 <niraj.bothe@locobuzz.com>
2025-06-03 00:32:09 +0530

    reactive forms

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,22 +1,50 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule } from '@angular/forms';
+import {
+  EmailValidator,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-  contact = {
+  selectedForm = ''; // default
+  reactiveForm: FormGroup;
+  templateSubmitted = false;
+  reactivesubmitted = false;
+  templateContact = {
     name: '',
     email: '',
     message: '',
   };
-  submitted = false;
 
-  onSubmit() {
-    this.submitted = true;
+  constructor(private fb: FormBuilder) {
+    this.reactiveForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+    });
+  }
+  selectForm(type: 'template' | 'reactive') {
+    this.selectedForm = type;
+    this.templateSubmitted = false;
+    this.reactiveForm.reset();
+  }
+  onTemplateSubmit() {
+    this.templateSubmitted = true;
+  }
+  onReactiveSubmit() {
+    if (this.reactiveForm.valid) {
+      this.reactivesubmitted = true;
+    }
   }
 }
